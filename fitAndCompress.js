f530ec6d26b4cd81165e7467a07e3efa3c97f0ab
Michał Kownacki <mk@michalkownacki.pl>
2022-03-10 01:29:04 +0100

    Rename option 'cover' in fitAndCompress to 'cover-with-clip'. 'cover' now does't clip.

--- fitAndCompress.js
+++ fitAndCompress.js
@@ -1,13 +1,19 @@
 const calculateNewSize = (fit, maxWidth, maxHeight, srcWidth, srcHeight) => {
   let newSize;
-  if (fit === 'cover') {
+  if (fit === 'cover-with-clip') {
     newSize = {
       width: Math.min(srcWidth, maxWidth),
       height: Math.min(srcHeight, maxHeight),
     }
-  } else { // 'scale-down'
-    // Calculate biggest new size that keeps aspect ratio and fits
-    const resizeRatio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight, 1);
+  } else { // 'cover' or 'scale-down'
+    const widthRatio = maxWidth / srcWidth;
+    const heightRatio = maxHeight / srcHeight;
+    let resizeRatio = (fit === 'cover')
+      ? Math.max(widthRatio, heightRatio)
+      // 'scale-down'
+      : Math.min(widthRatio, heightRatio);
+    // Prevent enlarging image. Image can only get smaller.
+    resizeRatio = Math.min(resizeRatio, 1);
     newSize = {width: srcWidth * resizeRatio, height: srcHeight * resizeRatio};
   }
   return newSize;
@@ -24,20 +30,20 @@ const cropWithAspectRatio = (newWidth, newHeight, srcWidth, srcHeight) => {
     y: (srcHeight - cropSize.height) / 2
   }
 };
-const calculateCropAndResize = (fit, maxWidth, maxHeight, srcWidth, srcHeight) => {
+export const calculateCropAndResize = (fit, maxWidth, maxHeight, srcWidth, srcHeight) => {
   const newSize = calculateNewSize(fit, maxWidth, maxHeight, srcWidth, srcHeight);
   return {
     newSize,
-    crop: fit === 'cover'
+    crop: fit === 'cover-with-clip'
       ? cropWithAspectRatio(newSize.width, newSize.height, srcWidth, srcHeight)
-      // 'scale-down'
+      // 'cover' or 'scale-down'
       : {x: 0, y: 0, width: srcWidth, height: srcHeight},
   };
 };
 
 // Returns promise with data URL containing resized and compressed image
 // Based on goo.gl/DEP8Y
-// fit - 'cover' or 'scale-down'
+// fit - 'cover-with-clip', 'cover', or 'scale-down'
 // quality - Quality of compressed jpeg image. Number between 0 and 1
 export const fitAndCompress = async (fit = 'scale-down', maxWidth = Infinity, maxHeight = Infinity, quality, imageFile) => {
   const img = document.createElement('img');
