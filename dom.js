12d91b51c71256c7fb7920173395f15616dab718
Michał Kownacki <mk@michalkownacki.pl>
2022-03-29 23:01:17 +0200

    Add isElementInProximity function.

--- dom.js
+++ dom.js
@@ -1,4 +1,8 @@
-export const isElementVisible = (element) => {
+export const isElementInProximity = (element, proximity = window.innerHeight) => {
   const rect = element.getBoundingClientRect();
-  return !(rect.top >= window.innerHeight) && !(rect.bottom <= 0);
+  return !(rect.top >= window.innerHeight + proximity) && !(rect.bottom <= -proximity);
+};
+
+export const isElementVisible = (element) => {
+  return isElementInProximity(element, 0);
 };
