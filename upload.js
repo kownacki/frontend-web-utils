8c2c1c07e2165475e766d5c283069fa7e00ffd51
Michał Kownacki <mk@michalkownacki.pl>
2022-05-26 14:44:59 +0200

    Simplify upload.

--- upload.js
+++ upload.js
@@ -2,25 +2,26 @@ import {sleep} from 'mk-js-utils';
 
 // Opens file upload window and resolves when user finishes upload.
 // Depending on user's choice:
-// 1. If user chose a file to upload, returns a promise with that file.
-// 2. If user closes window without uploading a file, returns a promise with false.
+// 1. If user chooses a file to upload, returns a promise with that file.
+// 2. If user cancels by closing the upload window without uploading a file, returns a promise with false.
+// We assume that upload was canceled if 300ms lasted after focusing window
+// see https://bit.ly/2V0F6DB
 export const upload = async () => {
   return new Promise((resolve) => {
     const input = document.createElement('input');
     input.type = 'file';
     input.accept = 'image/png, image/jpeg';
+
     // Resolve as soon as a file is uploaded
-    const changeHandler = (event) => {
+    input.addEventListener('change', (event) => {
       const file = event.target.files[0];
-      event.target.value = '';
       resolve(file);
-    };
-    input.addEventListener('change', changeHandler, {once: true});
+    });
+
     // Wait at most 300ms until assuming upload was canceled
     window.addEventListener('focus', async () => {
       await sleep(300);
       if (!input.files.length) {
-        input.removeEventListener('change', changeHandler);
         resolve(false);
       }
     }, {once: true});
